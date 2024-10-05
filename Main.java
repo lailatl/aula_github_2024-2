2b99c30956de00475c999a0b5d094860e1cc3ec0
Laila <lailatl2003@gmail.com>
2024-10-05 18:21:15 -0300

    Adicionados ajustes do Main e as transacoes

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner s = new Scanner(System.in);
-		ArrayList<Conta> contas = new ArrayList<>();
+		Banco banco = new Banco();
 
-		Menu mainMenu =  new Menu("\nMenu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
-		Menu contaMenu = new Menu("\nMenu Conta", Arrays.asList("Cadastrar"));
-
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+		Menu contaMenu = new Menu("Menu Conta", Arrays.asList("Cadastrar", "Consultar Saldo", "Consultar Histórico", "Visualizar"));
+		Menu clientMenu = new Menu("Menu Cliente", Arrays.asList("Cadastrar", "Visualizar"));
+		Menu operacoesMenu = new Menu("Menu Operações", Arrays.asList("Saque", "Depósito", "Tranferência", "Pagar fatura"));
 		int opMainMenu = mainMenu.getSelection();
 
 		while(opMainMenu != 4) {
@@ -19,34 +20,47 @@ public class Main {
 					int opContaMenu = contaMenu.getSelection();
 					switch(opContaMenu) {
 							case 1:
-								int numero, agencia;
-								String tipo;
-
-								System.out.println("\nCadastro de conta\n");
-								System.out.println("Informe o numero da conta: ");
-								numero = Integer.parseInt(s.nextLine());
-
-								System.out.println("\nInforme a agencia: ");
-								agencia = Integer.parseInt(s.nextLine());
-
-								System.out.println("\nInforme o tipo da conta: ");
-								tipo = s.nextLine();
-
-								contas.add(new Conta(numero, agencia, tipo));
-								System.out.println("\nConta cadastrada com sucesso!");
+								banco.cadastrarConta();
+							case 2:
+								banco.consultarSaldo();
 								break;
-							default:
+							case 3:
+								banco.visualizarHistorico();
 								break;
+							case 4:
+								banco.visualizarConta();
+								break;	
 						}
 					break;
 				case 2:
-					System.out.println("Cliente");
+					int opClientMenu = clientMenu.getSelection();
+					switch (opClientMenu) {
+						case 1:
+							banco.cadastrarCliente();
+							break;
+						case 2:
+							banco.visualizarCliente();
+							break;
+					}
 					break;
 				case 3:
-					System.out.println("Operacoes");
+					int opOperacoesMenu = operacoesMenu.getSelection();
+					switch (opOperacoesMenu) {
+						case 1:
+							banco.fazerSaque();
+							break;
+						case 2:
+							banco.fazerDeposito();
+							break;
+						case 3:
+							banco.fazerTranferencia();
+							break;
+						case 4:
+							banco.pagarFatura();
+							break;
+					}
 					break;
-				default:
-					break;
+		
 			}
 			opMainMenu = mainMenu.getSelection();
 		}
